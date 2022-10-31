17fb9d2e2cde072f306d05f1d50bd4fe47c85a90
Pamelar10 <116229630+Pamelar10@users.noreply.github.com>
2022-10-30 23:01:10 -0400

    updates on api

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 function displayTemperature(response) {
 let temperatureElement = documnet.querySelector("#temperature");
 temperatureElement.innerHTML = Math.round (response.data.main.temp);
+cityElement.innerHTML = response.data.name;
   
 }
 
 
 
 let apiKey = "b9338fa434bf42227o1029bt48f71d21";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?q=New York&key=b9338fa434bf42227o1029bt48f71d21&units=metric`;
+let apiUrl = "https://api.shecodes.io/weather/v1/current?query=Lisbon&key=b9338fa434bf42227o1029bt48f71d21&units=metric";
+
 
 
 axios.get(apiUrl).then(displayTemperature);
