684f87f3370be037cc1d1487ffbfc380171ea9d4
Pamelar10 <116229630+Pamelar10@users.noreply.github.com>
2022-10-31 00:30:14 -0400

    added humidity

--- src/app.js
+++ src/app.js
@@ -3,8 +3,10 @@ function displayTemperature(response) {
 let temperatureElement = document.querySelector("#temperature");
 let cityElement = document.querySelector("#city");
 let descriptionElement = document.querySelector("#description");
+let humidityElement = document.querySelector("#humidity");
 temperatureElement.innerHTML = Math.round(response.data.main.temp);
 cityElement.innerHTML = response.data.name;
+humidityElement.innerHTML = response.data.name;
 descriptionElement.innerHTML = response.data.weather[0].description;
   
 }
